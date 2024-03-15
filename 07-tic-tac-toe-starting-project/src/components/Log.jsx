831bdbd300c4098a0e65aa7821efd5a7d7f72cd8
Irwin Eguiluz <ijea92@gmail.com>
2024-03-15 14:06:39 -0500

    printing game turns on Log component (ep 85)

--- 07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,5 +1,16 @@
-export default function Log() {
+export default function Log({turns}) {
   return (
-    <ol></ol>
+    <ol id="log">
+      {turns.map((turn) => {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        return (
+          <li key={`${row}${col}`}>
+            {player} selected row: {row + 1}, col: {col + 1}
+          </li>
+        );
+      })}
+    </ol>
   );
 }
