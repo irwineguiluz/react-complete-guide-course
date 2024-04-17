2b83d52fb585fcd62baafbe3173a0f8629b2a896
Irwin Eguiluz <ijea92@gmail.com>
2024-04-17 18:40:10 -0500

    using two-way-binding again to control a value (ep 130)

--- ref-and-portals/src/components/Player.jsx
+++ ref-and-portals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [ inputtedPlayerName, setInputtedPlayerName ] = useState('');
+  const [ isSubmittedPlayerName, setIsSubmittedPlayerName ] = useState(false);
+
+  function handleChange(e) {
+    setIsSubmittedPlayerName(false);
+    setInputtedPlayerName(e.target.value);
+  }
+
+  function handleClick() {
+    setIsSubmittedPlayerName(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmittedPlayerName ? inputtedPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={inputtedPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
