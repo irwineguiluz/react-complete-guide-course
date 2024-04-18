338f7a2bd4094ad0fa9257236a94204b877920dc
Irwin Eguiluz <ijea92@gmail.com>
2024-04-18 18:12:34 -0500

    add settimeout and state values (ep 136)

--- ref-and-portals/src/components/TimerChallenge.jsx
+++ ref-and-portals/src/components/TimerChallenge.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
+
 export default function TimerChallenge({title, targetTime}) {
-    return (
-      <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-          {`${targetTime} second${targetTime > 1 ? 's' : ''}`}
-        </p>
-        <p>
-          <button>
-            Start Challenge
-          </button>
-        </p>
-        <p className="">
-          Time is running
-        </p>
-      </section>
-    );
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  function handleStart() {
+    setTimerStarted(true);
+    setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timeExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {`${targetTime} second${targetTime > 1 ? 's' : ''}`}
+      </p>
+      <p>
+        <button onClick={handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running' : 'Timer inactive'}
+      </p>
+    </section>
+  );
 }
