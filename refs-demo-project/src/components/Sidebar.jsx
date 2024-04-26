4580078810c664ec4bded58440250d0ad97f0701
Irwin Eguiluz <ijea92@gmail.com>
2024-04-26 17:05:40 -0500

    add styling to projects list items

--- refs-demo-project/src/components/Sidebar.jsx
+++ refs-demo-project/src/components/Sidebar.jsx
@@ -10,7 +10,13 @@ export default function Sidebar({onAddProject, savedProjects}) {
         <Button label="+ Add a project" onClick={onAddProject} />
       </div>
       <ul className="mt-8">
-        {savedProjects.map(project => <li key={project.id}>{project.title}</li>)}
+        {savedProjects.map(project => (
+          <li key={project.id} className="">
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
       </ul>
     </aside>
   );
