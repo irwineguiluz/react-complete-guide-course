8ba61bbe483e4212310ef1326907b777f6aad0bf
Irwin Eguiluz <ijea92@gmail.com>
2025-03-04 13:58:38 -0500

    add onClose method to fix close issue

--- food-order/src/components/UI/Modal.jsx
+++ food-order/src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({ children, isOpen, className = '' }) {
+export default function Modal({ children, isOpen, onClose, className = '' }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -15,7 +15,7 @@ export default function Modal({ children, isOpen, className = '' }) {
   }, [isOpen]);
 
   return createPortal(
-    <dialog className={`modal ${className}`} ref={dialog}>
+    <dialog className={`modal ${className}`} ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
