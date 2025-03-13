f14f1227db5e01276cabf79ef6ba00ea4d2a551e
Irwin Eguiluz <ijea92@gmail.com>
2025-03-13 17:49:04 -0500

    add clear cart data function

--- food-order/src/store/cart-context.jsx
+++ food-order/src/store/cart-context.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -46,6 +47,12 @@ function cartReducer(state, action) {
 
     return { ...state, items: updatedItems };
   }
+
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, items: [] };
+  }
+
+  return state;
 }
 
 export function CartContextProvider({ children }) {
@@ -59,10 +66,15 @@ export function CartContextProvider({ children }) {
     dispatchCartAction({ type: 'REMOVE_ITEM', id });
   }
 
+  function clearCart() {
+    dispatchCartAction({ type: 'CLEAR_CART' });
+  }
+
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart,
   };
 
   return (
