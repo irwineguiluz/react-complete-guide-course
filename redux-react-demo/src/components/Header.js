74fcfd02166a9e68124e7ad5c4a417e09594f6e4
Irwin Eguiluz <ijea92@gmail.com>
2025-04-24 13:45:25 -0500

    show/hide navbar items and set logout dispatch

--- redux-react-demo/src/components/Header.js
+++ redux-react-demo/src/components/Header.js
@@ -1,22 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { authActions } from '../store/index';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(authActions.setLogout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
