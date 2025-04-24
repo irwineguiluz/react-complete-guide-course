c61e8a72e8e98b430b7ae7eadfb7ac3f059f7f62
Irwin Eguiluz <ijea92@gmail.com>
2025-04-24 13:46:27 -0500

    show/hide auth related components

--- redux-react-demo/src/App.js
+++ redux-react-demo/src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth ? <Auth /> : <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
