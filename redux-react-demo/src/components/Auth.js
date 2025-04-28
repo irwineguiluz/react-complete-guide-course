08a1a9bc32f622fb7569314d02f0b8c4993bfd4a
Irwin Eguiluz <ijea92@gmail.com>
2025-04-28 16:28:16 -0500

    change button attribute event

--- redux-react-demo/src/components/Auth.js
+++ redux-react-demo/src/components/Auth.js
@@ -7,14 +7,16 @@ import classes from './Auth.module.css';
 const Auth = () => {
   const dispatch = useDispatch();
 
-  function handleLogin() {
+  function handleLogin(e) {
+    e.preventDefault();
+
     dispatch(authActions.setLogin());
   }
 
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
             <input type="email" id="email" />
@@ -23,9 +25,7 @@ const Auth = () => {
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <button type="button" onClick={handleLogin}>
-            Login
-          </button>
+          <button onClick={handleLogin}>Login</button>
         </form>
       </section>
     </main>
