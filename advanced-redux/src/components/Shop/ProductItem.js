3a53f93e60c16264fa79bf3afeeda9e579ec7548
Irwin Eguiluz <ijea92@gmail.com>
2025-04-30 08:39:33 -0500

    add addProductItem function to handle dispatch

--- advanced-redux/src/components/Shop/ProductItem.js
+++ advanced-redux/src/components/Shop/ProductItem.js
@@ -1,8 +1,17 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart';
+
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { id, title, price, description } = props;
+  const dispatch = useDispatch();
+
+  function handleAddProductItem() {
+    dispatch(cartActions.addItem({ id, title, price }));
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +22,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={handleAddProductItem}>Add to Cart</button>
         </div>
       </Card>
     </li>
