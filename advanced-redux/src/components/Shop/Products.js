da4f3169ce7fd3eb7b6313a0928656eaf9d3f8c0
Irwin Eguiluz <ijea92@gmail.com>
2025-04-30 08:36:36 -0500

    create dummy data (products) to map

--- advanced-redux/src/components/Shop/Products.js
+++ advanced-redux/src/components/Shop/Products.js
@@ -1,16 +1,26 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1', title: 'Test', price: 6, description: 'Test description' },
+  { id: 'p2', title: 'Test 2', price: 7, description: 'Test description' },
+  { id: 'p3', title: 'Test 3', price: 5, description: 'Test description' },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
