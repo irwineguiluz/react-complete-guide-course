566b959f1139e2161b2b829b6ec5948c5d05daf3
Irwin Eguiluz <ijea92@gmail.com>
2025-04-30 11:24:36 -0500

    add addItem and removeItem to CartItem component

--- advanced-redux/src/components/Cart/CartItem.js
+++ advanced-redux/src/components/Cart/CartItem.js
@@ -1,7 +1,19 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart';
+
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { id, title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  function handleAddItem() {
+    dispatch(cartActions.addItem({ id, title, price }));
+  }
+  function handleRemoveItem() {
+    dispatch(cartActions.removeItem(id));
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +29,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={handleRemoveItem}>-</button>
+          <button onClick={handleAddItem}>+</button>
         </div>
       </div>
     </li>
