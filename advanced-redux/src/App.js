a52f06543e83fa55eaa36da87156b6f866d0419b
Irwin Eguiluz <ijea92@gmail.com>
2025-05-07 17:09:28 -0500

    send a http put request to firebase (ep 333)

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const toggleCart = useSelector((state) => state.cart.toggleCart);
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    fetch('https://redux-example-5eb32-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {toggleCart && <Cart />}
